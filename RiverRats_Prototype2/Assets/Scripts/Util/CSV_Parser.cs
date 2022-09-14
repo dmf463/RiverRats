72a7b5c721bf4b067dee8cea7b702c9d6920eff6
David <davidfrisch.games@gmail.com>
2022-09-14 09:04:22 -0400

    Made it so I can break down the string from the CSV into RULE and TargetPlayers

--- RiverRats_Prototype2/Assets/Scripts/Util/CSV_Parser.cs
+++ RiverRats_Prototype2/Assets/Scripts/Util/CSV_Parser.cs
@@ -15,6 +15,30 @@ public class CSV_Parser : MonoBehaviour
     void Start()
     {
         ReadData();
+        int randomNum = Random.Range(0, rawRules.Length);
+        char[] text = rawRules[randomNum].ToCharArray();
+        string rule = rawRules[randomNum].Remove(rawRules[randomNum].IndexOf('('));
+        List<int> ruleTargets = new List<int>();
+        for(int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '0'|| text[i] == '1' || text[i] == '2' || text[i] == '3' || text[i] == '4')
+            {
+                ruleTargets.Add(GetSeatPos(text[i]));
+            }
+        }
+        if(ruleTargets.Count == 0)
+        {
+            Debug.Log("rule = " + rule);
+        }
+        else if(ruleTargets.Count == 1)
+        {
+            Debug.Log("Player " + ruleTargets[0] + " : " + rule);
+        }
+        else if(ruleTargets.Count == 2)
+        {
+            Debug.Log("Player " + ruleTargets[0] + " " + rule + " player " + ruleTargets[1]);
+        }
+
     }
 
     // Update is called once per frame
@@ -30,5 +54,18 @@ public class CSV_Parser : MonoBehaviour
         {
             Debug.Log(rawRules[i]);
         }
+    }
+
+    private int GetSeatPos(char c)
+    {
+        int pos;
+
+        if (c == '0') pos = 0;
+        else if (c == '1') pos = 1;
+        else if (c == '2') pos = 2;
+        else if (c == '3') pos = 3;
+        else pos = 4;
+
+        return pos;
     }
 }
